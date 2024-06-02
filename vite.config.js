542349b9741787729323f6651b47269c15af70ad
muhammadsyihab <muhammadsyihab406@gmail.com>
2024-06-03 00:20:00 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/e-book/",
+  base: "/reactjs_ebook/",
   optimizeDeps: {
     include: [
       'jquery',
